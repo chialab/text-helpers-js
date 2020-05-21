81feba2ba11b9d1d10d7ea5451bcd59797f36fbc
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2020-05-21 08:34:06 +0200

    feat: export text patches

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export { CharAnalyzer } from './char-analyzer.js';
 export { FontAnalyzer } from './font-analyzer.js';
 export { A11yText } from './a11y-text.js';
+export * from './chunk-patches.js';
 export { default as chunk } from './chunk.js';
 export { LineHeight } from './line-height.js';
